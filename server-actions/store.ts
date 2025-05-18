d07ed6896b1c4aa5c83289e3d789592271464a6b
Muhammad-Rafeh <m.rafeh77@gmail.com>
2025-05-19 03:27:32 +0500

    some chagnes

--- server-actions/store.ts
+++ server-actions/store.ts
@@ -2,8 +2,8 @@
 
 import { db } from "@/db/db";
 import { stores } from "@/db/schema";
-import { currentUser } from "@clerk/nextjs";
-import { users } from "@clerk/nextjs/dist/api";
+import { currentUser } from '@clerk/nextjs/server'
+import { users } from "@clerk/nextjs/api"
 import { eq, or } from "drizzle-orm";
 import { z } from "zod";
 import { createSlug } from "@/lib/createSlug";
